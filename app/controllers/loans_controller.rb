a4246e7f238c5d4825c459562f4638f6f9d9c448
fastman <fastman@b7b41519-fa4e-0410-9904-8e07f07a1bd8>
2008-06-11 17:16:44 +0000

    poprawione migracje + plugin autoryzacji 2
    git-svn-id: svn://fm.kei.pl/biblioteka/trunk@16 b7b41519-fa4e-0410-9904-8e07f07a1bd8

---
+++ app/controllers/loans_controller.rb
@@ -0,0 +1,34 @@
+class LoansController < ApplicationController
+  def destroy
+    user = User.first
+    @book = Book.find(params[:book_id])
+    loan = Loan.find(params[:id])
+
+    if loan.user == user
+      ss = @book.statuses.detect { |s| s.loan == loan }
+      ss.loan_id = nil
+      ss.save
+      loan.date_returned = Time.now
+      loan.save
+    end
+
+    flash[:notice] = "Ksiazka oddana"
+
+    redirect_to :controller => :books, :action => :show, :id => @book
+  end
+
+  def create
+    user = User.first
+    @book = Book.find(params[:book_id])
+    status = @book.available?
+
+    loan = Loan.create(:user => user, :book => @book, :date_issued => Time.now, :date_due_for_return => Time.now + 3.weeks)
+    status.loan = loan
+    status.save
+
+    flash[:notice] = "Ksiazka wypozyczona"
+
+    redirect_to :controller => :books, :action => :show, :id => @book
+  end
+
+end
