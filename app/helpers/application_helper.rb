f9d112828e49299f25622462bdd9b37548b1d1c1
fastman <fastman@b7b41519-fa4e-0410-9904-8e07f07a1bd8>
2008-06-19 13:30:03 +0000

    wersja 0.1
    git-svn-id: svn://fm.kei.pl/biblioteka/trunk@38 b7b41519-fa4e-0410-9904-8e07f07a1bd8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def shorten_name(name, len=nil)
+    out = ''
+    max_length = 60
+    max_length = len if !len.nil?
+
+    name.split.each { |word|
+
+      if (out.length + word.length + 3) > max_length
+        out += '...'
+        break
+      else
+        out += word + ' '
+      end
+    }
+    out.chomp    
+    out.sub(/\ \.\.\./, ' ...')
+  end
+
 end
